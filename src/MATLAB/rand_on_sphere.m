1730950dd81ca591981576be48cd4ad89265ccce
Tom Shlomo <tomshlomo@gmail.com>
2020-06-30 17:04:48 +0300

    added rand_on_sphere.m (MATLAB function)
    Former-commit-id: 07194a04b667786a52a8d53eb54f5b2a2b6f0524

---
+++ src/MATLAB/rand_on_sphere.m
@@ -0,0 +1,33 @@
+function varargout = rand_on_sphere(len, outputType)
+arguments
+    len (1,1) {mustBeNumeric, mustBeNonnegative, mustBeFinite} = 1;
+    outputType (1,1) string {mustBeMember(outputType,["cart","sphere"])} = "sphere"
+end
+
+
+ph  =  rand(len,1)*2*pi;
+v = rand(len,1);
+th = acos(2*v-1);
+
+switch outputType
+    case "sphere"
+        if nargout<=1
+            varargout = {[th ph]};
+        elseif nargout==2
+            varargout = {th,ph};
+        else
+            error("too many outputs")
+        end
+    case "cart"
+        [x, y, z] = s2c(th, ph);
+        if nargout<=1
+            varargout = {[x y z]};
+        elseif nargout==3
+            varargout = {x,y,z};
+        else
+            error("illegal number of outputs");
+        end
+end
+
+end
+
--- src/utils/loss_functions.py
+++ src/utils/loss_functions.py
@@ -2,7 +2,7 @@ import torch
 
 
 def l2_outer_product(x, target):
-    # x: (N, Q, L)
-    # target: (N, Q, Q)
+    # x: (N, 2, Q, L)
+    # target: (N, 2, Q, Q)
     d = target - torch.matmul(x, x.transpose(1, 2))
     return torch.sum(d ** 2)

7ce332af94858ceed92dab7f3a2d33838dca1f85
Tom Shlomo <tomshlomo@gmail.com>
2020-07-08 13:32:02 +0300

    implemented l2 outer product loss for complex matrices
    Former-commit-id: c078e81d91f457f3e91d7f8f47d4a758c3d66af7

--- src/utils/loss_functions.py
+++ src/utils/loss_functions.py
@@ -1,8 +1,57 @@
 import torch
 
 
-def l2_outer_product(x, target):
+def l2_outer_product(x: torch.Tensor, target: torch.Tensor) -> torch.Tensor:
     # x: (N, 2, Q, L)
     # target: (N, 2, Q, Q)
-    d = target - torch.matmul(x, x.transpose(1, 2))
-    return torch.sum(d ** 2)
+
+    N = x.shape[0]
+    x_tuple = get_real_imag_parts(x)
+    target_tuple = get_real_imag_parts(target)
+
+    outer_prod = complex_outer_product(x_tuple)
+    d_real = target_tuple[0] - outer_prod[0]
+    d_imag = target_tuple[1] - outer_prod[1]
+    return l2_sq_complex((d_real, d_imag))
+
+
+def l2_sq_complex(x) -> torch.Tensor:
+    # x: ((N, M, L), (N, M, L))
+    return torch.sum(x[0] ** 2 + x[1] ** 2) / x[0].shape[0]
+
+
+def get_real_imag_parts(x):
+    # x: (_, 2, _, _)
+
+    x_real = x[:, 0]
+    x_imag = x[:, 1]
+
+    return x_real, x_imag
+
+
+def complex_mm(x, y):
+    # x - ((N, M, L), (N, M, L))
+    # y - ((N, L, K), (N, L, K))
+    # out - ((N, M, K), (N, M, K))
+
+    x_real, x_imag = x
+    y_real, y_imag = y
+
+    out_real = torch.matmul(x_real, y_real) - torch.matmul(x_imag, y_imag)
+    out_imag = torch.matmul(x_real, y_imag) + torch.matmul(x_imag, y_real)
+
+    return out_real, out_imag
+
+
+def complex_outer_product(x):
+    # x - ((N, Q, L), (N, Q, L))
+    # out - ((N, Q, Q), (N, Q, Q))
+    x_real, x_imag = x
+
+    return complex_mm((x_real, x_imag), (x_real.transpose(1, 2), -x_imag.transpose(1, 2)))
+
+# def cat_real_imag_parts(x_real, x_imag):
+#     # x_real, x_imag - (N, M, L)
+#     # out - (N, 2, M, L)
+#
+#     return torch.cat(x_real, x_imag)
--- src/utils/test_loss_functions.py
+++ src/utils/test_loss_functions.py
@@ -2,11 +2,57 @@ from unittest import TestCase
 
 import torch
 
-from src.utils.loss_functions import l2_outer_product
+from src.utils.loss_functions import l2_outer_product, get_real_imag_parts, complex_mm
 
 
 class Test(TestCase):
     def test_l2_outer_product(self):
-        x = torch.ones(5, 4, 1)
-        target = torch.ones(5, 4, 4)
-        self.assertEqual(l2_outer_product(x, target), 0)
+        x = torch.zeros((3, 2, 2, 1))
+        x[0, 0] = torch.tensor([[1], [0]])
+        x[0, 1] = torch.tensor([[0], [1]])
+        x[1, 0] = torch.tensor([[0], [3]])
+
+        target = torch.zeros((3, 2, 2, 2))
+        target[0, 0] = torch.tensor([[1,  0], [0, 1]])
+        target[0, 1] = torch.tensor([[0, -1], [1, 0]])
+        target[1, 0] = torch.tensor([[0, 0], [0, 9]])
+
+        loss = l2_outer_product(x, target)
+        self.assertEqual(loss, 0)
+
+    def test_get_real_imag_parts(self):
+        x = torch.arange(3 * 2 * 4 * 5).reshape((3, 2, 4, 5))
+        x_real, x_imag = get_real_imag_parts(x)
+        self.assertTrue(x_real.shape == torch.Size((3, 4, 5)))
+        self.assertTrue(x_imag.shape == torch.Size((3, 4, 5)))
+
+
+    def test_complex_mm(self):
+        x_real = torch.tensor([[[1, 0],
+                                [0, 1]],
+                               [[2, 0],
+                                [0, 2]]])
+        x_imag = torch.tensor([[[0, 0],
+                                [0, 0]],
+                               [[0, 1],
+                                [1, 0]]])
+        y_real = torch.tensor([[[1],
+                                [0]],
+                               [[0],
+                                [0]]])
+        y_imag = torch.tensor([[[0],
+                                [1]],
+                               [[1],
+                                [0]]])
+        out_real = torch.tensor([[[1],
+                                  [0]],
+                                 [[0],
+                                  [-1]]])
+        out_imag = torch.tensor([[[0],
+                                  [1]],
+                                 [[2],
+                                  [0]]])
+        out_real_test, out_imag_test = complex_mm((x_real, x_imag), (y_real, y_imag))
+        self.assertTrue(torch.equal(out_real_test, out_real))
+        self.assertTrue(torch.equal(out_imag_test, out_imag))
+

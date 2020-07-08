fixed mse loss so it will be averaged properly
Former-commit-id: 467d8f19785e26e64ada4b15ac93a4b094ec64ef

src/drafts/train_fc_lightning.py
@@ -16,12 +16,12 @@ config = {
     "input_size": 512, # TODO: fix hardcoding
     "output_size": 4802, # TODO: fix hardcoding
     "output_shape": [2,49,49], # TODO: fix hardcoding
-    "hidden_layers": 3,
-    "hidden_sizes": [1900,2500,3200],
+    "hidden_layers": 1,
+    "hidden_sizes": [1900],
     # ---data---
     # "dtype": torch.float32, # TODO: implement (does errors in saving hyperparameters)
     "transform": None,
-    "batch_size": 3,
+    "batch_size": 30,
     "num_workers": 6,
     "train_val_split": [0.9,0.1],
     # ---optimization---

src/models/fc_model.py
@@ -47,7 +47,7 @@ class BaseModelLT(LightningModule):
         sizes = [opts['input_size'], *opts['hidden_sizes'], opts['output_size']]
         self.linears = nn.ModuleList([nn.Linear(in_size, out_size)
                                       for in_size, out_size in zip(sizes, sizes[1:])])
-        self.loss = nn.MSELoss()
+        self.loss = lambda x,y: nn.MSELoss()(x,y) * np.prod(x.shape[1:])
 
     def forward(self, x):
         # REQUIRED
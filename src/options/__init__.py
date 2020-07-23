4cfd27d9d01b059b766dc832a9f21c4958319a8c
Ran <ranwsn@gmail.com>
2020-07-23 15:19:41 +0300

    some small changes and modifications to the options parsing. Added also a validation function, that checks if the options are valid
    Former-commit-id: 44f74d84565cf136f781bb1fbeee32f58d679079

--- src/options/__init__.py
+++ src/options/__init__.py
@@ -1,8 +1,12 @@
 import json
 import os
 import sys
+import warnings
 
+import torch
 import yaml
+
+from src.models import find_model_using_name
 
 
 class EasyDict(dict):
@@ -47,24 +51,48 @@ def update_opts_with_flags(opts):
     return opts
 
 
-def prepare_opts(default_config, config=None, with_flags=True, print_flag=True):
+def prepare_opts(base_opts, opts=None, with_flags=True, print_flag=False):
     # open and parse in case of files
-    if type(default_config) == str:
-        default_config = EasyDict(read_yaml(default_config))
-    if type(config) == str:
-        config = EasyDict(read_yaml(config))
+    if type(base_opts) == str:
+        base_opts = EasyDict(read_yaml(base_opts))
+    if type(opts) == str:
+        opts = EasyDict(read_yaml(opts))
     # in case of no config input
-    elif config is None:
-        config = default_config
+    elif opts is None:
+        opts = base_opts
 
     # update config according to defaults and command-line flags
-    config = update_opts_with_defaults(config, default_config)
+    opts = update_opts_with_defaults(opts, base_opts)
     if with_flags:
-        config = update_opts_with_flags(config)
+        opts = update_opts_with_flags(opts)
 
     # print
     if print_flag:
-        print("Training parameters: ")
-        print("\n".join([k + ": " + str(v) for k, v in config.items()]))
+        print_opts(opts)
 
-    return config
+    return dict(opts)
+
+def validate_opts(opts,print_flag=True):
+    ''' validate that the given options are OK, and perform some automatic fixes if needed'''
+
+    # GPU
+    if ~torch.cuda.is_available() and opts['gpus']!=0:
+        warnings.warn('GPU is not available, using CPU instead')
+        opts['gpus'] = 0
+
+    # print
+    if print_flag:
+        print_opts(opts)
+
+    return opts
+
+def get_default_opts(opts):
+    ''' find the default options of the given model'''
+    if type(opts) == str:
+        opts = EasyDict(read_yaml(opts))
+    _,default_opts = find_model_using_name(opts['model_name'])
+    return default_opts
+
+def print_opts(opts):
+    print("Configuration Parameters: ")
+    print("\n".join([k + ": " + str(v) for k, v in opts.items()]))

041100e2c3f802aea1ef9a50ce9ddbc6106f8e1a
Ran <ranwsn@gmail.com>
2020-07-29 17:02:32 +0300

    added an option to continue data generation from a given index
    Former-commit-id: 74ccdb88e6660e03f05448a2635d4314ee0ec1db

--- src/data/MATLAB/make_image_method_data.m
+++ src/data/MATLAB/make_image_method_data.m
@@ -1,12 +1,17 @@
-function file_path = make_image_method_data(num_of_files)
+function file_path = make_image_method_data(num_of_files,start_index)
 %UNTITLED6 Summary of this function goes here
 %   Detailed explanation goes here
+
+arguments
+    num_of_files
+    start_index = 1
+end
 
 SELECTED_FREQ = 500;
 folder_path = fullfile(get_raw_data_folder_path(), "image-method");
 file_path = strings(num_of_files,1);
 wb = wbar();
-for i=1:num_of_files
+for i=start_index:num_of_files
     %%
     [~, anm_target, fs] = simulator(i, "roomIndex", 2);
 
@@ -16,7 +21,7 @@ for i=1:num_of_files
     R = R(:,:,j);
     
     v = [real(R(:)).'; imag(R(:)).'];
-    file_path(i) = fullfile(folder_path, sprintf("%04d.bin", i)); 
+    file_path(i) = fullfile(folder_path, sprintf("%08d.bin", i)); 
     fid = fopen(file_path(i), 'w');
     fwrite(fid, v(:), 'double');
     wbar(i,num_of_files,wb);

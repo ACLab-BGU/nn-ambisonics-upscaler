cd6a63d6c45c8664aac33e9dc048f085a582f888
Ran <ranwsn@gmail.com>
2020-07-30 00:41:47 +0300

    small fixes
    Former-commit-id: 1ef5ca7b72f367dd30ec4f6303ee85090ac6c716

--- src/train.py
+++ src/train.py
@@ -7,16 +7,17 @@ from src.utils import prepare_logger, prepare_trainer
 
 def train(opts):
     base_opts = options.get_default_opts(opts)
-    opts = options.prepare_opts(base_opts,opts)
-    opts = options.validate_opts(opts)
+    full_opts = options.prepare_opts(base_opts,opts)
+    full_opts = options.validate_opts(full_opts)
 
-    model = prepare_model(opts)
-    logger = prepare_logger(opts)
-    trainer = prepare_trainer(opts,logger)
+    model = prepare_model(full_opts)
+    logger = prepare_logger(full_opts)
+    trainer = prepare_trainer(full_opts,logger)
 
     trainer.fit(model)
 
 
 if __name__ == '__main__':
+    assert len(sys.argv) > 1, "Please insert a config file to run"
     opts = sys.argv[1]
     train(opts)

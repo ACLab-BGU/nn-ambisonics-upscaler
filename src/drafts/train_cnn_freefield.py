38a4b7222d03dd322ee6d7b74015933ad813fedf
Tom Shlomo <tomshlomo@gmail.com>
2020-08-13 11:28:02 +0300

    drafts

--- src/drafts/train_cnn_freefield.py
+++ src/drafts/train_cnn_freefield.py
@@ -1,5 +1,6 @@
 from src.train import train
 
+hidden_layers = 0
 train({"model_name":"cnn",
        "data_path": r"/Users/tomshlomo/PycharmProjects/nn-ambisonics-upscaler2/data/whitenoise_10_reflections",
        "experiment_name": "cnn_0_refs_deep_3_to_4_hiddens_1_kernel_1_channels_500_10_refs",
@@ -8,11 +9,11 @@ train({"model_name":"cnn",
        "lr": 0.001,
        "lr_sched_patience": 3,
        "lr_sched_thresh": 0.1,
-       "hidden_layers": 3,
-       "kernel_widths": [1]*4,
-       "strides": [1]*4,
-       "hidden_channels": [1000, 1000, 2000],
+       "hidden_layers": hidden_layers,
+       "kernel_widths": [1]*(hidden_layers+1),
+       "strides": [1]*(hidden_layers+1),
+       "hidden_channels": [],
        "sh_order_sig": 3,
-       "sh_order_scm": 4,
+       "sh_order_scm": 3,
        "gpus": 0,
        })

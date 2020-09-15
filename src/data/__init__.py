e481c2f5f462da0a1276bcbe2d27a1765bc42f06
Ran <ranwsn@gmail.com>
2020-09-15 21:47:15 +0300

    added support for non-compact SCMs

--- src/data/__init__.py
+++ src/data/__init__.py
@@ -12,14 +12,15 @@ from scipy import io as sio
 
 num_of_reflections = [10]
 source_type = ["whitenoise"]  # whitenoise/speech
-num_of_files = 5
-sig_length = 0.5
-target_sh_order = 5
+num_of_files = 100
+sig_length = 2
+target_sh_order = 4
+compact_cov = False
 path_data = os.path.join("..", "..", "data")
-output_format = "mat"  # pickle/mat/npz
+output_format = "npz"  # pickle/mat/npz
 
 
-def gen_data(num_of_reflections, source_type, num_of_files, path_data, output_format):
+def gen_data(num_of_reflections, source_type, compact_cov, num_of_files, path_data, output_format):
     '''generate data using matlab'''
     eng = matlab.engine.start_matlab()
     eng.cd('MATLAB')
@@ -34,7 +35,7 @@ def gen_data(num_of_reflections, source_type, num_of_files, path_data, output_fo
 
             # generate file with matlab and save to mat file
             eng.make_image_method_data(num_of_files, 1, "folder_path", folder_path, "number_of_reflections", refs,
-                                       "source_type", sig, "duration", sig_length, "target_sh_order", target_sh_order)
+                                       "source_type", sig, "duration", sig_length, "target_sh_order", target_sh_order,"compact_cov", compact_cov)
 
             # convert files to a better format
             # TODO: check more efficient alternatives, instead of saving mat files and then converting
@@ -46,7 +47,13 @@ def convert_mat_files(folder_path, output_format):
 
     path_list = glob.glob(os.path.join(folder_path, "*.mat"))
     for path_old in path_list:
+        # load file
         data = sio.loadmat(path_old)
+
+        # change memory order to python convention
+        for key in data:
+            if type(data[key]) == np.ndarray:
+                data[key] = np.ascontiguousarray(data[key])
         path_new = os.path.join(os.path.dirname(path_old), Path(path_old).stem)
 
         if output_format == "mat":
@@ -83,12 +90,20 @@ def load_data_file(file_name, cache=None):
         raise NotImplementedError
 
     # some post-processing
-    R_vecs = d['R'].transpose()
-    Q = np.sqrt(R_vecs.shape[1]).astype(int)
-    F = d['freq'].shape[0]
-    d['R'] = np.zeros((F, Q, Q), dtype=np.cdouble)
-    for f, R_vec in enumerate(R_vecs):
-        d['R'][f, :, :], cache = vectorized_cov_to_mat(R_vec, cache)
+    if d['R'].ndim == 2:
+        is_cov_compact = True
+    else:
+        is_cov_compact = False
+
+    if is_cov_compact:
+        R_vecs = d['R'].transpose()
+        Q = np.sqrt(R_vecs.shape[1]).astype(int)
+        F = d['freq'].shape[0]
+        d['R'] = np.zeros((F, Q, Q), dtype=np.cdouble)
+        for f, R_vec in enumerate(R_vecs):
+            d['R'][f, :, :], cache = vectorized_cov_to_mat(R_vec, cache)
+    else:
+        cache = None
     d['R'] = d['R'] / d['R_scaling']
     d['anm'] = d['anm'].astype(np.double) / d['anm_scaling']
     d['nfft'] = int(d['nfft'])
@@ -125,4 +140,4 @@ def vectorized_cov_to_mat(v, cache=None):
 
 if __name__ == '__main__':
     gen_data(num_of_reflections=num_of_reflections, source_type=source_type, num_of_files=num_of_files,
-             path_data=path_data, output_format=output_format)
+             path_data=path_data, output_format=output_format, compact_cov=compact_cov)
